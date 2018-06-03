72b87133c87f4fbd6fe97b9f122160e09347560d
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-06-03 16:44:10 -0500

    props children component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/layout';
 
 
-class CampaignIndex  extends Component {
+class CampaignIndex extends Component {
     /* getInitioalProps(), para renderizar desde el servidor Next.js */
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
@@ -13,7 +14,7 @@ class CampaignIndex  extends Component {
 
 
     renderCampaigns() {
-       
+
         /* Usando map https://reactjs.org/docs/lists-and-keys.html */
         const items = this.props.campaigns.map(address => {
             return {
@@ -26,10 +27,20 @@ class CampaignIndex  extends Component {
         return <Card.Group items={items} />
     }
     render() {
-        return <div>
-           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
-        {this.renderCampaigns()}
-        </div>
+        return (
+        <Layout> 
+            <div>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
+                <h3>Open Campaigns</h3>
+                {this.renderCampaigns()}
+                <Button
+                content="Create Campaign"
+                icon="add circle"
+                primary
+                />
+            </div>
+        </Layout>
+        );
     }
 }
 
