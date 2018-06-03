354709a23132f3834d277f5253d2da2371f8f02e
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-06-03 18:38:44 -0500

    creat campaign page

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+/* Introduce cualquier contenido o elemento dentro de la etiqueta <head> de html */
+import Head from 'next/head';
+import Header from './header';
+
+
+
 
 export default props => {
     return (
-        <div>
-            <h1> I am header</h1>
+        <Container>
+            <Head>
+                <link 
+                rel="stylesheet" 
+                href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" 
+                />
+            </Head>
+            <Header />
             {props.children}
-            <h1> I am footer</h1>
-        </div>
+        </Container>
     );
 }
