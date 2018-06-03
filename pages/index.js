18d1e4623837faaaa0beff654fcd2b3d2cf57364
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-06-03 16:08:31 -0500

    cdnjs for semantic css

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 
 class CampaignIndex  extends Component {
-
-    state = {
-        campaigns: ''
-    }
-
-    async componentDidMount() {
+    /* getInitioalProps(), para renderizar desde el servidor Next.js */
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
         console.log(campaigns);
-        this.setState({ campaigns:campaigns});
+        return { campaigns };
     }
 
+
+    renderCampaigns() {
+       
+        /* Usando map https://reactjs.org/docs/lists-and-keys.html */
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items} />
+    }
     render() {
-        return <div>{this.state.campaigns}</div>
+        return <div>
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
+        {this.renderCampaigns()}
+        </div>
     }
 }
 
 export default CampaignIndex;
+
