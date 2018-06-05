3022972dd1e9c62b732efa3d0318c1b85d8edabf
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-06-05 11:21:36 -0500

    show campaign page

---
+++ pages/campaigns/show.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, Grid, Button } from 'semantic-ui-react';
+import Layout from '../../components/layout';
+import web3 from '../../ethereum/web3';
+import { Link } from '../../routes';
+
+class CampaignShow extends Component {
+    render() {
+        return (
+            <Layout>
+                <h3>Show Campaing</h3>
+            </Layout>
+
+        )
+    }
+}
+
+export default CampaignShow;
--- pages/index.js
+++ pages/index.js
@@ -8,45 +8,47 @@ class CampaignIndex extends Component {
     /* getInitioalProps(), para renderizar desde el servidor Next.js */
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns);
+    
         return { campaigns };
-    }
-
+      }
 
     renderCampaigns() {
 
-        /* Usando map https://reactjs.org/docs/lists-and-keys.html */
         const items = this.props.campaigns.map(address => {
-            return {
-                header: address,
-                description: <a>View Campaign</a>,
-                fluid: true
-            }
+          return {
+            header: address,
+            description: (
+              <Link route={`/campaigns/${address}`}>
+                <a>View Campaign</a>
+              </Link>
+            ),
+            fluid: true
+          };
         });
-
-        return <Card.Group items={items} />
-    }
+    
+        return <Card.Group items={items} />;
+      }
     render() {
         return (
-        <Layout> 
-            <div>
-                <h3>Open Campaigns</h3>
-                    
+            <Layout>
+                <div>
+                    <h3>Open Campaigns</h3>
+
                     <Link route="/campaigns/new">
-                    <a>
-                        <Button
-                        floated="right"
-                        content="Create Campaign"
-                        icon="add circle"
-                        primary
-                        />
-                    </a>
+                        <a>
+                            <Button
+                                floated="right"
+                                content="Create Campaign"
+                                icon="add circle"
+                                primary
+                            />
+                        </a>
                     </Link>
 
 
-                {this.renderCampaigns()}
-            </div>
-        </Layout>
+                    {this.renderCampaigns()}
+                </div>
+            </Layout>
         );
     }
 }
