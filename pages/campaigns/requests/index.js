c9bafcb617fc4e4eac8d146ff2248851d9a642fd
Julio Gutierrez <julgq@iMac-de-Julio.local>
2018-06-11 14:12:20 -0500

    requests array

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
     static async getInitialProps(props){
         const { address } = props.query;
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestCount().call();
 
-        return { address }
+        /* Creamos un array en base al numero de requestCount, en solidity no se puede iterar un array de Struct */
+        const requests = await Promise.all(
+            Array(parseInt(requestCount))
+          .fill()
+          .map((element, index) => {
+              return campaign.methods.requests(index).call();
+          })
+        );
+        console.log(requests);
+        return { address, requests, requestCount };
     }
 
     render() {
